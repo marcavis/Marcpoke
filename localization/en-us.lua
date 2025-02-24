a2fa117b7a2987240a2f3d9bfe888d87c922739e
marcavis <marcavis@yahoo.com.br>
2025-02-23 22:56:00 -0300

    add Conkeldurr line

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -18,6 +18,42 @@ return {
                     "remaining cards in deck"
                 } 
             },
+            j_marcpoke_timburr = {
+                name = "Timburr",
+                text = {
+                    "If scoring hand has at least two of",
+                    "{C:attention}Stone{}, {C:attention}Steel{} or {C:attention}Glass{} cards,",
+                    "gain {C:money}$#1#{}",
+                    "{C:inactive,s:0.8}(Evolves at $#2#{C:inactive,s:0.8}/{C:money,s:0.8}$#3#{C:inactive,s:0.8} earned)",
+                } 
+            },
+            j_marcpoke_gurdurr = {
+                name = "Gurdurr",
+                text = {
+                    "If scoring hand has at least two of",
+                    "{C:attention}Stone{}, {C:attention}Steel{} or {C:attention}Glass{} cards,",
+                    "gain {C:money}$#1#{}",
+                    "{C:inactive,s:0.8}(Evolves at $#2#{C:inactive,s:0.8}/{C:money,s:0.8}$#3#{C:inactive,s:0.8} earned)",
+                } 
+            },
+            j_marcpoke_conkeldurr = {
+                name = "Conkeldurr",
+                text = {
+                    "If scoring hand has at least two of",
+                    "{C:attention}Stone{}, {C:attention}Steel{} or {C:attention}Glass{} cards, gain {C:money}$#1#{}",
+                    "{br:2}text needs to be here to work",
+                    "3+ {C:attention}Stone{} cards: Gain +1 Joker slot",
+                    "3+ {C:attention}Steel{} cards: Gain +1 consumable slot",
+                    "3+ {C:attention}Glass{} cards: Gain +1 booster slot",
+                } 
+            },
         }
     }
 }
+
+-- text = {
+--     "Played cards with",
+--     "{C:diamonds}#3#{} suit give",
+--     "{C:mult}+#1#{} Mult and ",
+--     "{C:money}$#2#{} when scored",
+-- } 
--- pokemon/wpokejokers_19.lua
+++ pokemon/wpokejokers_19.lua
@@ -91,8 +91,167 @@ local garbodor = {
     end
   }
 -- Zorua 570
+
+local timburr = {
+    name = "timburr", 
+    pos = {x = 6, y = 5}, 
+    config = {extra = {money = 2, earned = 0, dollars_required = 8}},
+    loc_vars = function(self, info_queue, center)
+      type_tooltip(self, info_queue, center)
+      return {vars = {center.ability.extra.money, center.ability.extra.earned, center.ability.extra.dollars_required}}
+    end,
+    rarity = 2, 
+    cost = 4,
+    ptype = "Fighting",
+    stage = "Basic", 
+    atlas = "marcPoke5",
+    enhancement_gate = 'm_stone',
+    blueprint_compat = true,
+    calculate = function(self, card, context)
+        if context.cardarea == G.jokers and context.scoring_hand then
+            if context.joker_main then
+                local material_count = 0
+                for i = 1, #context.scoring_hand do
+                    if context.scoring_hand[i].ability.name == 'Stone Card' or
+                    context.scoring_hand[i].ability.name == 'Steel Card' or
+                    context.scoring_hand[i].ability.name == 'Glass Card' then
+                        if not context.scoring_hand[i].debuff then
+                            material_count = material_count + 1    
+                        end
+                    end
+                end
+                if material_count >= 2 then
+                    local earned = ease_poke_dollars(card, "timburr", card.ability.extra.money, true)
+                    card.ability.extra.earned = card.ability.extra.earned + earned
+                    return {
+                        dollars = earned
+                    }
+                end
+            end
+        end
+        return scaling_evo(self, card, context, "j_marcpoke_gurdurr", card.ability.extra.earned, card.ability.extra.dollars_required)
+      end,
+  }
+
+  local gurdurr = {
+    name = "gurdurr", 
+    pos = {x = 7, y = 5}, 
+    config = {extra = {money = 3, earned = 0, dollars_required = 20}},
+    loc_vars = function(self, info_queue, center)
+      type_tooltip(self, info_queue, center)
+      return {vars = {center.ability.extra.money, center.ability.extra.earned, center.ability.extra.dollars_required}}
+    end,
+    rarity = 2, 
+    cost = 4,
+    ptype = "Fighting",
+    stage = "One", 
+    atlas = "marcPoke5",
+    enhancement_gate = 'm_stone',
+    blueprint_compat = true,
+    calculate = function(self, card, context)
+        if context.cardarea == G.jokers and context.scoring_hand then
+            if context.joker_main then
+                local material_count = 0
+                for i = 1, #context.scoring_hand do
+                    if context.scoring_hand[i].ability.name == 'Stone Card' or
+                    context.scoring_hand[i].ability.name == 'Steel Card' or
+                    context.scoring_hand[i].ability.name == 'Glass Card' then
+                        if not context.scoring_hand[i].debuff then
+                            material_count = material_count + 1    
+                        end
+                    end
+                end
+                if material_count >= 2 then
+                    local earned = ease_poke_dollars(card, "gurdurr", card.ability.extra.money, true)
+                    card.ability.extra.earned = card.ability.extra.earned + earned
+                    return {
+                        dollars = earned
+                    }
+                end
+            end
+        end
+        return scaling_evo(self, card, context, "j_marcpoke_conkeldurr", card.ability.extra.earned, 18)
+    end
+  }
+
+  local conkeldurr = {
+    name = "conkeldurr", 
+    pos = {x = 8, y = 5}, 
+    config = {extra = {
+        money = 4, earned = 0,
+        stone_quest = false, steel_quest = false, glass_quest = false,
+        stone_reward = false, steel_reward = false, glass_reward = false
+    }},
+    loc_vars = function(self, info_queue, center)
+      type_tooltip(self, info_queue, center)
+      return {vars = {center.ability.extra.money, center.ability.extra.earned}}
+    end,
+    rarity = "poke_safari", 
+    cost = 4,
+    ptype = "Fighting",
+    stage = "Two", 
+    atlas = "marcPoke5",
+    enhancement_gate = 'm_stone',
+    blueprint_compat = true,
+    calculate = function(self, card, context)
+        if context.cardarea == G.jokers and context.scoring_hand then
+            if context.joker_main then
+                local material_count = 0
+                local materials = {
+                    ['Stone Card'] = 0,
+                    ['Steel Card'] = 0,
+                    ['Glass Card'] = 0
+                }
+                for i = 1, #context.scoring_hand do
+                    local cardtype = context.scoring_hand[i].ability.name
+                    if not context.scoring_hand[i].debuff then
+                        if cardtype == "Stone Card" or cardtype == "Steel Card" or cardtype == "Glass Card" then
+                            materials[cardtype] = materials[cardtype] + 1
+                            material_count = material_count + 1
+                        end
+                    end
+                end
+                if materials['Stone Card'] >= 3 then card.ability.extra.stone_quest = true end
+                if materials['Steel Card'] >= 3 then card.ability.extra.steel_quest = true end
+                if materials['Glass Card'] >= 3 then card.ability.extra.glass_quest = true end
+                if material_count >= 2 then
+                    local earned = ease_poke_dollars(card, "conkeldurr", card.ability.extra.money, true)
+                    card.ability.extra.earned = card.ability.extra.earned + earned
+                    return {
+                        dollars = earned
+                    }
+                end
+            end
+        elseif context.cardarea == G.jokers and context.end_of_round then
+            if card.ability.extra.stone_quest and not card.ability.extra.stone_reward then
+                card.ability.extra.stone_reward = true
+                G.jokers.config.card_limit = G.jokers.config.card_limit + 1
+                return {
+                    message = "House Built!"
+                }
+            elseif card.ability.extra.steel_quest and not card.ability.extra.steel_reward then
+                card.ability.extra.steel_reward = true
+                G.consumeables.config.card_limit = G.consumeables.config.card_limit + 1
+                return {
+                    message = "Storeroom Built!"
+                }
+            elseif card.ability.extra.glass_quest and not card.ability.extra.glass_reward then
+                card.ability.extra.glass_reward = true
+                if G.GAME.modifiers.poke_booster_packs then
+                    G.GAME.modifiers.poke_booster_packs = G.GAME.modifiers.poke_booster_packs + 1
+                else
+                    G.GAME.modifiers.poke_booster_packs = 3
+                end
+                return {
+                    message = "Shop Built!"
+                }
+            end
+        end
+    end
+  }
+
 return {name = "Pokemon Jokers 541-570", 
-        list = {trubbish, garbodor},
+        list = {trubbish, garbodor, timburr, gurdurr, conkeldurr},
 }
 
 -- local kakuna={
@@ -121,4 +280,32 @@ return {name = "Pokemon Jokers 541-570",
 --       end
 --       return level_evo(self, card, context, "j_poke_beedrill")
 --     end,
+--   }
+
+-- local starmie={
+--     name = "starmie", 
+--     pos = {x = 3, y = 9},
+--     config = {extra = {mult = 4, money_mod = 1, suit = "Diamonds"}},
+--     loc_vars = function(self, info_queue, center)
+--       type_tooltip(self, info_queue, center)
+--       return {vars = {center.ability.extra.mult, center.ability.extra.money_mod, localize(center.ability.extra.suit, 'suits_singular')}}
+--     end,
+--     rarity = "poke_safari", 
+--     cost = 10, 
+--     stage = "One", 
+--     ptype = "Water",
+--     atlas = "Pokedex1",
+--     blueprint_compat = true,
+--     calculate = function(self, card, context)
+--       if context.individual and context.cardarea == G.play and context.other_card:is_suit(card.ability.extra.suit) then
+--         if not context.end_of_round and not context.before and not context.after and not context.other_card.debuff then
+--           local earned = ease_poke_dollars(card, "starmie", card.ability.extra.money_mod, true)
+--           return {
+--             mult = card.ability.extra.mult,
+--             dollars = earned,
+--             card = card
+--           }
+--         end
+--       end
+--     end
 --   }
