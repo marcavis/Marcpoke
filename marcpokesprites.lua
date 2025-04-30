7411d77a4ce6390a2ea9b962c03bd65535461589
marcavis <marcavis@yahoo.com.br>
2025-04-30 20:06:15 -0300

    sprite reorganization

---
+++ marcpokesprites.lua
@@ -0,0 +1,201 @@
+SMODS.Atlas({
+    key = "marcpokeboss",
+    atlas_table = "ANIMATION_ATLAS",
+    frames = 21,
+    path = "marcpokeboss.png",
+    px = 34,
+    py = 34
+  }):register()
+  
+  SMODS.Atlas({
+    key = "super_nerd",
+    path = "SuperNerd.png",
+    px = 71,
+    py = 95
+  }):register()
+  
+  SMODS.Atlas({
+    key = "marcpoke_mom",
+    path = "mom.png",
+    px = 71,
+    py = 95
+  }):register()
+  
+  SMODS.Atlas({
+    key = "regionals",
+    path = "Regionals.png",
+    path_prefix = SMODS.Mods["Pokermon"].path,
+    px = 71,
+    py = 95
+  }):register()
+  
+  SMODS.Atlas({
+    key = "shiny_regionals",
+    path = "ShinyRegionals.png",
+    path_prefix = SMODS.Mods["Pokermon"].path,
+    px = 71,
+    py = 95
+  }):register()
+  
+  SMODS.Atlas({
+    key = "marcPoke1",
+    path = "Pokedex1.png",
+    path_prefix = SMODS.Mods["Pokermon"].path,
+    px = 71,
+    py = 95
+  }):register()
+  
+  SMODS.Atlas({
+    key = "shiny_marcPoke1",
+    path = "Shinydex1.png",
+    path_prefix = SMODS.Mods["Pokermon"].path,
+    px = 71,
+    py = 95
+  }):register()
+  
+  SMODS.Atlas({
+    key = "marcPoke2",
+    path = "Pokedex2.png",
+    path_prefix = SMODS.Mods["Pokermon"].path,
+    px = 71,
+    py = 95
+  }):register()
+  
+  SMODS.Atlas({
+    key = "shiny_marcPoke2",
+    path = "Shinydex2.png",
+    path_prefix = SMODS.Mods["Pokermon"].path,
+    px = 71,
+    py = 95
+  }):register()
+  
+  SMODS.Atlas({
+    key = "marcPoke3",
+    path = "Pokedex3.png",
+    path_prefix = SMODS.Mods["Pokermon"].path,
+    px = 71,
+    py = 95
+  }):register()
+  
+  SMODS.Atlas({
+    key = "shiny_marcPoke3",
+    path = "Shinydex3.png",
+    path_prefix = SMODS.Mods["Pokermon"].path,
+    px = 71,
+    py = 95
+  }):register()
+  
+  SMODS.Atlas({
+    key = "marcPoke4",
+    path = "Pokedex4.png",
+    path_prefix = SMODS.Mods["Pokermon"].path,
+    px = 71,
+    py = 95
+  }):register()
+  
+  SMODS.Atlas({
+    key = "shiny_marcPoke4",
+    path = "Shinydex4.png",
+    path_prefix = SMODS.Mods["Pokermon"].path,
+    px = 71,
+    py = 95
+  }):register()
+  
+  SMODS.Atlas({
+      key = "marcPoke5",
+      path = "Pokedex5.png",
+      path_prefix = SMODS.Mods["Pokermon"].path,
+      px = 71,
+      py = 95
+  }):register()
+  
+  SMODS.Atlas({
+    key = "shiny_marcPoke5",
+    path = "Shinydex5.png",
+    path_prefix = SMODS.Mods["Pokermon"].path,
+    px = 71,
+    py = 95
+  }):register()
+  
+  SMODS.Atlas({
+    key = "marcPoke6",
+    path = "Pokedex6.png",
+    path_prefix = SMODS.Mods["Pokermon"].path,
+    px = 71,
+    py = 95
+  }):register()
+  
+  SMODS.Atlas({
+    key = "shiny_marcPoke6",
+    path = "Shinydex6.png",
+    path_prefix = SMODS.Mods["Pokermon"].path,
+    px = 71,
+    py = 95
+  }):register()
+  
+  SMODS.Atlas({
+    key = "marcPoke7",
+    path = "Pokedex7.png",
+    path_prefix = SMODS.Mods["Pokermon"].path,
+    px = 71,
+    py = 95
+  }):register()
+  
+  SMODS.Atlas({
+    key = "shiny_marcPoke7",
+    path = "Shinydex7.png",
+    path_prefix = SMODS.Mods["Pokermon"].path,
+    px = 71,
+    py = 95
+  }):register()
+  
+  SMODS.Atlas({
+    key = "marcPoke8",
+    path = "Pokedex8.png",
+    path_prefix = SMODS.Mods["Pokermon"].path,
+    px = 71,
+    py = 95
+  }):register()
+  
+  SMODS.Atlas({
+    key = "shiny_marcPoke8",
+    path = "Shinydex8.png",
+    path_prefix = SMODS.Mods["Pokermon"].path,
+    px = 71,
+    py = 95
+  }):register()
+  
+  SMODS.Atlas({
+    key = "luminous_cave",
+    path = "LuminousCave.png",
+    px = 71,
+    py = 95
+  }):register()
+  
+  SMODS.Atlas({
+    key = "shiny_luminous_cave",
+    path = "LuminousCave.png",
+    px = 71,
+    py = 95
+  }):register()
+  
+  SMODS.Atlas({
+    key = "bills_pc",
+    path = "BillsPC.png",
+    px = 71,
+    py = 95
+  }):register()
+  
+  SMODS.Atlas({
+    key = "shiny_bills_pc",
+    path = "BillsPC.png",
+    px = 71,
+    py = 95
+  }):register()
+  
+  SMODS.Atlas {
+      key = "stickers",
+      path = "stickers.png",
+      px = 71,
+      py = 95
+  }
